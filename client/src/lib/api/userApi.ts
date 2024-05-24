c0c6867920dfba2012b1c02e74ab250b7e796b84
kntWT <kento.wata0628@gmail.com>
2024-05-24 15:38:03 +0900

    fix: apiをproxyするようにした

--- client/src/lib/api/userApi.ts
+++ client/src/lib/api/userApi.ts
@@ -1,8 +1,6 @@
 import { UserApi } from "./generated";
 import axios from "axios";
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-});
+const axiosInstance = axios.create({});
 
 export const userApi = new UserApi(undefined, undefined, axiosInstance);
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		},
+		server: {
+			host: true,
+			proxy: {
+				'/api/': {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api\//, '/')
+				}
+			}
+		}
 	}
 });
