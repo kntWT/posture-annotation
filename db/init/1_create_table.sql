7d92ae64c7c8377fba18ec3b3decad4d4ad66e3d
kntWT <kento.wata0628@gmail.com>
2024-06-04 18:48:14 +0900

    fi: 姿勢を管理するテーブル追加

--- db/init/1_create_table.sql
+++ db/init/1_create_table.sql
@@ -11,6 +11,38 @@ CREATE TABLE users (
     updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE postures (
+    id SERIAL PRIMARY KEY,
+    in_id INTEGER NOT NULL,
+    file_name TEXT NOT NULL,
+    set_num INTEGER NOT NULL,
+    orientation_alpha REAL NOT NULL,
+    orientation_beta REAL NOT NULL,
+    orientation_gamma REAL NOT NULL,
+    pitch REAL NOT NULL,
+    yaw REAL NOT NULL,
+    roll REAL NOT NULL,
+    nose_x REAL NOT NULL,
+    nose_y REAL NOT NULL,
+    neck_x REAL NOT NULL,
+    neck_y REAL NOT NULL,
+    neck_to_nose REAL NOT NULL,
+    standard_dist REAL NOT NULL,
+    in_created_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    in_updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    neck_to_nose_standard REAL NOT NULL,
+    neck_angle_offset REAL NOT NULL,
+    ex_id INTEGER NOT NULL,
+    neck_angle REAL NOT NULL,
+    torso_angle REAL NOT NULL,
+    ex_created_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    annotater_id INTEGER,
+    updated_at TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -21,5 +53,10 @@ $$ language 'plpgsql';
 
 CREATE TRIGGER update_users_updated_at
 BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TRIGGER update_postures_updated_at
+BEFORE UPDATE ON postures
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
