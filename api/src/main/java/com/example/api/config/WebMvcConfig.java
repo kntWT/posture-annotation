f064d8bdbde3c83837c9b068a21c18f7c972c284
kntWT <kento.wata0628@gmail.com>
2024-06-12 10:01:40 +0900

    fix(api): CORSの設定

--- api/src/main/java/com/example/api/config/WebMvcConfig.java
+++ api/src/main/java/com/example/api/config/WebMvcConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
     }
 }
