44d9e7d83f381b83bd6d773ef9a616a91fc7f470
kntWT <kento.wata0628@gmail.com>
2024-07-12 18:57:51 +0900

    fix(client): Felteがデフォルトのsubmitイベントをオーバーライドしてしまうのでfetch処理追加

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -2,15 +2,47 @@
 	import Form from "./Form.svelte";
     import Divider from "$lib/components/Divider.svelte";
 	import type { ActionData } from "./$types";
-	import { goto } from "$app/navigation";
+	import { goto, invalidateAll } from "$app/navigation";
 	import { onMount } from "svelte";
+	import type { UserCreate } from "$api/generated";
 
     export let form: ActionData
+
+    let isMounted = false;
     onMount(() => {
         if (form?.status === 200) {
             goto("/");
         }
+        isMounted = true;
     });
+
+    const handleSubmit = (action: "login" | "signup") => {
+        return async (values: UserCreate) => {
+            const formData = new FormData();
+            formData.append("name", values.name);
+            formData.append("password", values.password);
+            // FIXME: 全てのコンポーネントをリロードせずに再レンダリングしたい
+            await fetch(`?/${action}`, {
+                method: "POST",
+                body: formData,
+            })
+            .then(res => res.json())
+            .then((res) => {
+                console.log(res)
+                if (res.status === 200) {
+                    window.location.reload();
+                } else {
+                    const data = JSON.parse(res.data);
+                    const message = data[data[0].message];
+                    alert(message);
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+                alert("エラーが発生しました")
+            })
+        }
+    }
 </script>
 
 <div class="wrapper">
@@ -20,9 +52,14 @@
             <p>{form.message}</p>
         </div>
     {/if}
-    <Form title="ログイン" action="login" />
-    <Divider color={"gray"} />
-    <Form title="新規登録" action="signup" />
+    <!-- NOTE: felteなどがSSR対応してないので，mount後にレンダリング -->
+    {#if !isMounted}
+        <p>読み込み中...</p>
+    {:else}
+        <Form title="ログイン" action="login" handleSubmit={handleSubmit("login")} />
+        <Divider color={"gray"} />
+        <Form title="新規登録" action="signup" handleSubmit={handleSubmit("signup")} />
+    {/if}
 </div>
 
 <style lang="scss">
