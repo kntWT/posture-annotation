30c0686610a8c5e5d0e3a88257633d219c79a9cd
kntWT <kento.wata0628@gmail.com>
2024-07-12 16:10:13 +0900

    chore(client): smui, felte, zod導入（Form対応）

--- client/src/routes/login/Form.svelte
+++ client/src/routes/login/Form.svelte
@@ -1,57 +1,83 @@
 <script lang="ts">
+    import Textfield from "@smui/textfield";
+    import Button from "@smui/button";
+    import { z } from "zod";
+    import { createForm } from "felte";
+    import { validator } from "@felte/validator-zod"
+    import { reporter } from '@felte/reporter-svelte';
+	import type { UserCreate } from "$api/generated";
+    import ValidationMessage from "./ValidationMessage.svelte";
+
     export let title: string;
     export let action: string;
+    export let handleSubmit: (values: UserCreate) => Promise<unknown>;
 
-    let name = "";
-    let password = "";
+    const schema = z.object({
+        name: z.string()
+            .trim()
+            .min(1, { message: "名前を入力してください" })
+            .max(20, { message: "20文字以内で入力してください" }),
+        password: z.string()
+            .trim()
+            .min(1, { message: "パスワードを入力してください" })
+            .max(20, { message: "20文字以内で入力してください" }),
+    });
 
-    $: isSubmittable = name.length > 0 && password.length > 0;
+    type schemaType = z.infer<typeof schema>;
+
+    const initialValues: UserCreate = {
+        name: "",
+        password: "",
+    };
+
+    const { form, data, isValid } =  createForm<schemaType>({
+        initialValues,
+        onSubmit: async (values) => {
+            await handleSubmit(values);
+        },
+        extend: [ validator({ schema }), reporter],
+    });
 </script>
 
 <div class="form">
     <h1>{title}</h1>
-    <form method="post" action={`?/${action}`}>
+    <form
+        use:form
+        method="post"
+        action={`?/${action}`}
+    >
         <div class="form-group">
-            <label for={`${action}-name`}>ユーザ名</label>
-            <input type="text" name="name" id={`${action}-name`} bind:value={name} required>
+            <Textfield
+                label="ユーザ名"
+                bind:value={$data.name}
+            />
+            <ValidationMessage name="name" />
+            <Textfield
+                label="パスワード"
+                type="password"
+                style="suggested: current-password;"
+                bind:value={$data.password}
+            />
+            <ValidationMessage name="password" />
         </div>
-        <div class="form-group">
-            <label for={`${action}-password`}>パスワード</label>
-            <input type="password" name="password" id={`${action}-password`} bind:value={password} required>
-        </div>
-        <button disabled={!isSubmittable}>ログイン</button>
+        <Button disabled={!$isValid} variant="raised" type="submit" >{title}</Button>
     </form>
 </div>
 
 <style lang="scss">
-    @import "$lib/styles/variables";
-
     .form {
         margin: 20px;
-    }
-
-    .form-group {
-        width: fit-content;
-        margin: 10px auto;
-
-        label {
-            display: inline-block;
-            width: 100px;
-        }
-    }
-
-    h1 {
         text-align: center;
     }
 
-    button {
-        margin: 20px auto;
-        display: block;
-        width: 20%;
-        height: 2rem;
-        &:not(:disabled) {
-            background-color: $base-color;
-            border: none;
-        }
+    .form-group {
+        width: 80%;
+        min-width: 200px;
+        max-width: 400px;
+        margin: 24px auto;
+    }
+
+    :global(.mdc-text-field) {
+        width: 100%;
     }
 </style>
---
+++ client/src/routes/login/ValidationMessage.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { ValidationMessage } from '@felte/reporter-svelte';
+
+    export let name: string;
+</script>
+
+<ValidationMessage for={name} let:messages={message}>
+    <div class="error-info">{message || ""}</div>
+</ValidationMessage>
+
+<style lang="scss">
+    @import "$lib/styles/variables";
+    .error-info {
+        color: $error-color;
+        font-size: 0.8rem;
+    }
+</style>
