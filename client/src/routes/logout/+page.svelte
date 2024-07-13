153aa1b7a5ef01a66621a3a9772aa2835a59f3db
kntWT <kento.wata0628@gmail.com>
2024-07-13 18:24:58 +0900

    fix(client): 再レンダリングされないので，クライアントサイドでリダイレクト

---
+++ client/src/routes/logout/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { logout } from '$lib/store/user';
+	import { goto } from '$app/navigation';
+
+    onMount(() => {
+        logout();
+        goto("/");
+    });
+</script>
