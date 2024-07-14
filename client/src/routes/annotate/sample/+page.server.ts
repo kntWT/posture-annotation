5a9d9c7762267b6bb2506dc8e396109898816ee0
kntWT <kento.wata0628@gmail.com>
2024-07-14 21:32:34 +0900

    feat(client): サンプルデータ用のアノテーション画面実装

---
+++ client/src/routes/annotate/sample/+page.server.ts
@@ -0,0 +1,24 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { createPostureApi } from "$api";
+
+
+export const load: PageServerLoad = async ({ cookies, url }) => {
+    const token = cookies.get(`${import.meta.env.VITE_COOKIE_PREFIX}token`);
+    if (!token || token === '' || token === 'undefined') {
+        redirect(301, '/');
+    }
+    const id = url.searchParams.get("id") ?? null;
+    const postureApi = createPostureApi({ token });
+    try {
+        if (id && /^[0-9]+$/.test(id)) {
+            const posture = await postureApi.getPostureById({ id: parseInt(id) });
+            return { posture };
+        }
+        const posture = await postureApi.getRandomSamplePosture();
+        return { posture };
+    } catch (e) {
+        console.error(e);
+        return { posture: null };
+    }
+}
---
+++ client/src/routes/annotate/sample/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { PageData } from "./$types";
+	import Annotate from "../Annotate.svelte";
+	import { goto } from "$app/navigation";
+
+    export let data: PageData;
+
+    onMount(() => {
+        console.log(data)
+    });
+
+    const handlePostUndo = (dst: number) => {
+        const path = window.location.pathname;
+        // FIXME: なぜかinvalidateAllしてもページが更新されないので，リソーそを無理矢理変更している
+        data = { ...data, posture: null };
+        goto(`${path}?id=${dst}`, { invalidateAll: true });
+    }
+
+    const onSuccess = () => {
+        const path = window.location.pathname;
+        goto(path, { invalidateAll: true });
+        data = { ...data, posture: null };
+    }
+
+    const onError = (e?: Error) => {
+        console.error(e);
+        // FIXME: なぜか再レンダリングしても破棄されたPostureAnnotaterのsubmitが呼ばれてしまうので，無理矢理リセット
+        window.location.href = `/annotate?id=${data.posture?.id}`
+        // alert("データの送信に失敗しました");
+    }
+
+</script>
+
+<Annotate
+    posture={data.posture}
+    user={data.user}
+    handlePostUndo={handlePostUndo}
+    onSuccess={onSuccess}
+    onError={onError}
+/>
