b41e6b5b8f765526395ada4959e9960cfb881b0c
kntWT <kento.wata0628@gmail.com>
2024-07-14 21:22:45 +0900

    feat(api): アプリ起動時にサンプルデータ生成

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.api.services.PostureService;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	@Autowired
+	private static PostureService postureService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		Long dataSize = Long.parseLong(System.getenv("INITIAL_SAMPLE_DATA_SIZE"));
+		postureService.createSamplePostures(dataSize);
 	}
 
 }
