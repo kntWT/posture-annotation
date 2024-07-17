05d5a3fb4320abdc5fced1b0c0ac526b637820e0
kntWT <kento.wata0628@gmail.com>
2024-07-17 15:48:33 +0900

    fix(client): 遷移先のパスを絶対パスに

--- client/src/lib/components/Header.svelte
+++ client/src/lib/components/Header.svelte
@@ -43,13 +43,13 @@
         <Section align="end" toolbar>
             {#if $page.url.pathname !== "/login"}
                 {#if $user}
-                    <Button on:click={() => navigateTo("logout")} variant="raised">
+                    <Button on:click={() => navigateTo("/logout")} variant="raised">
                         <Label>
                             ログアウト
                         </Label>
                     </Button>
                 {:else }
-                    <Button on:click={() => navigateTo("login")} variant="raised">
+                    <Button on:click={() => navigateTo("/login")} variant="raised">
                         <Label>
                             ログイン
                         </Label>
