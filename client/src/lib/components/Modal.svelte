ec2add7a8a4c7d9620468d01c4a239a5e02bc8d3
kntWT <kento.wata0628@gmail.com>
2024-07-18 16:46:26 +0900

    feat(client): モーダル実装

---
+++ client/src/lib/components/Modal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let open: boolean = false;
+    export let handleClose: () => void;
+</script>
+
+{#if open}
+    <div
+        class="modal"
+        on:click={handleClose}
+    >
+        <slot />
+    </div>
+{/if}
+
+<style lang="scss">
+    .modal {
+        position: fixed;
+        z-index: 100;
+        left: 0;
+        top: 0;
+        width: 100vw;
+        height: 100vh;
+        overflow: scroll;
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+</style>
