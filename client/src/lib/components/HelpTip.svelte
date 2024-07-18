372d178726d9202884bad1c8144e07a6512ea921
kntWT <kento.wata0628@gmail.com>
2024-07-18 16:46:43 +0900

    feat(client): ヘルプボタン実装

---
+++ client/src/lib/components/HelpTip.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import IconButton from "@smui/icon-button";
+
+    export let handleClick: () => void;
+
+</script>
+
+<div class="wrapper">
+    <IconButton class="material-icons" on:click={handleClick}>
+        help
+    </IconButton>
+</div>
+
+<style lang="scss">
+    .wrapper {
+        width: 100vw;
+        :global(.mdc-icon-button) {
+            position: absolute;
+            left: auto;
+            right: 0;
+            bottom: 0;
+            margin: 16px;
+        }
+    }
+</style>
