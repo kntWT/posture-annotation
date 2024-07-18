07f14ba7499b4248ce3f630cdb25626f8dca79f7
kntWT <kento.wata0628@gmail.com>
2024-07-18 18:42:12 +0900

    fix(client): モーダルを@smui/dialogを用いるように

--- client/src/lib/components/Modal.svelte
+++ client/src/lib/components/Modal.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
-    export let open: boolean = false;
+    import Dialog, { Actions, Header } from "@smui/dialog";
+    import IconButton from "@smui/icon-button";
+	import Paper from "@smui/paper";
+
+    export let open: boolean;
     export let handleClose: () => void;
+
 </script>
 
 {#if open}
-    <div
-        class="modal"
-        on:click={handleClose}
+    <Dialog
+        bind:open
+        on:SMUIDialog:closed={handleClose}
     >
-        <slot />
-    </div>
+        <Paper>
+            <Header>
+                <Actions>
+                    <IconButton on:click={handleClose} class="material-icons">close</IconButton>
+                </Actions>
+            </Header>
+            <slot />
+        </Paper>
+    </Dialog>
 {/if}
 
-<style lang="scss">
-    .modal {
-        position: fixed;
-        z-index: 100;
-        left: 0;
+<style lang="scss" scoped>
+    :global(.mdc-dialog .mdc-icon-button) {
+        position: absolute;
+        right: 0;
         top: 0;
-        width: 100vw;
-        height: 100vh;
-        overflow: scroll;
-        background-color: rgba(0, 0, 0, 0.4);
+        margin: 16px;
     }
 </style>
