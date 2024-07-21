85418a75cf28e477eaf96992951abe45d5bb5206
kntWT <kento.wata0628@gmail.com>
2024-07-21 16:45:01 +0900

    fix(client): モーダルを開いている間はsubmitできないように モーダルが開いている間もp5のmousePressedイベントが発火してしまうの，userをnullにしsubmitできないように

--- client/src/routes/annotate/+page.svelte
+++ client/src/routes/annotate/+page.svelte
@@ -3,8 +3,13 @@
     import type { PageData } from "./$types";
 	import Annotate from "./Annotate.svelte";
 	import { goto } from "$app/navigation";
+	import HelpTip from "$lib/components/HelpTip.svelte";
+	import Modal from "$lib/components/Modal.svelte";
+	import HowToAnnotate from "$lib/components/HowToAnnotate.svelte";
 
     export let data: PageData;
+
+    let openHelpModal = false;
 
     onMount(() => {
         console.log(data)
@@ -30,12 +35,29 @@
         // alert("データの送信に失敗しました");
     }
 
+    const handleOpenHelpModal = () => {
+        openHelpModal = true;
+    }
+
+    const handleCloseHelpModal = () => {
+        openHelpModal = false;
+    }
+
+    $: user = openHelpModal ? null : data.user;
+
 </script>
 
+<!-- FIXME: モーダルが開いている時にもp5のmousePressedイベントが発火してしまうので，userをnullにして送信できなくする -->
+{#key user}
 <Annotate
     posture={data.posture}
-    user={data.user}
+    user={user}
     handlePostUndo={handlePostUndo}
     onSuccess={onSuccess}
     onError={onError}
 />
+{/key}
+<Modal open={openHelpModal} handleClose={handleCloseHelpModal}>
+    <HowToAnnotate />
+</Modal>
+<HelpTip handleClick={handleOpenHelpModal} />
