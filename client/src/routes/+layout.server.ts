4efb52aeb2a62631484463431365f72f7ff06d38
kntWT <kento.wata0628@gmail.com>
2024-09-23 19:03:02 +0900

    refactor(client): prettier

--- client/src/routes/+layout.server.ts
+++ client/src/routes/+layout.server.ts
@@ -3,17 +3,17 @@ import { userApi } from '$api';
 import { toBearer } from '$lib/util';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
-    const token = cookies.get(`${import.meta.env.VITE_COOKIE_PREFIX}token`);
-    if (!token || token === '') {
-        return {
-            user: null
-        }
-    }
+	const token = cookies.get(`${import.meta.env.VITE_COOKIE_PREFIX}token`);
+	if (!token || token === '') {
+		return {
+			user: null
+		};
+	}
 
-    try {
-        const user = await userApi.getUserByToken({ authorization: toBearer(token) });
-        return { user };
-    } catch (e) {
-        return { user: null };
-    }
-}
+	try {
+		const user = await userApi.getUserByToken({ authorization: toBearer(token) });
+		return { user };
+	} catch (e) {
+		return { user: null };
+	}
+};
