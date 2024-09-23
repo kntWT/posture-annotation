c8694c88eb9be7708e2b86f6a0c284e317a0c49b
kntWT <kento.wata0628@gmail.com>
2024-09-23 09:57:07 +0900

    refactor(api): クエリに改行をいれる

--- api/src/main/java/com/example/api/repositories/AnnotationRepository.java
+++ api/src/main/java/com/example/api/repositories/AnnotationRepository.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import jakarta.transaction.Transactional;
 
 import com.example.api.entities.AnnotationEntity;
-
-import jakarta.transaction.Transactional;
+import com.generated.model.AnnotationSummary;
 
 @Repository
 public interface AnnotationRepository extends JpaRepository<AnnotationEntity, Long> {
@@ -26,109 +26,130 @@ public interface AnnotationRepository extends JpaRepository<AnnotationEntity, Lo
 
     @Modifying
     @Transactional
-    @Query(
-        value = "UPDATE annotations SET tragus_x = :tragusX, tragus_y = :tragusY, shoulder_x = :shoulderX, shoulder_y = :shoulderY, waist_x = :waistX, waist_y = :waistY, neck_angle = :neckAngle, torso_angle = :torsoAngle WHERE id = :id",
-        nativeQuery = true
-    )
+    @Query(value = """
+            UPDATE annotations SET
+            tragus_x = :tragusX,
+            tragus_y = :tragusY,
+            shoulder_x = :shoulderX,
+            shoulder_y = :shoulderY,
+            waist_x = :waistX,
+            waist_y = :waistY,
+            neck_angle = :neckAngle,
+            torso_angle = :torsoAngle
+            WHERE id = :id
+            """, nativeQuery = true)
     public int updateAnnotationById(
-        @Param("id") Long id,
-        @Param("tragusX") Double tragusX,
-        @Param("tragusY") Double tragusY,
-        @Param("shoulderX") Double shoulderX,
-        @Param("shoulderY") Double shoulderY,
-        @Param("waistX") Double waistX,
-        @Param("waistY") Double waistY,
-        @Param("neckAngle") Double neckAngle,
-        @Param("torsoAngle") Double torsoAngle
-    );
+            @Param("id") Long id,
+            @Param("tragusX") Double tragusX,
+            @Param("tragusY") Double tragusY,
+            @Param("shoulderX") Double shoulderX,
+            @Param("shoulderY") Double shoulderY,
+            @Param("waistX") Double waistX,
+            @Param("waistY") Double waistY,
+            @Param("neckAngle") Double neckAngle,
+            @Param("torsoAngle") Double torsoAngle);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p ON a.posture_id = p.id WHERE a.id = :id",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p ON a.posture_id = p.id
+            WHERE a.id = :id
+            """, nativeQuery = true)
     public AnnotationEntity findByIdWithFilePath(@Param("id") Long id);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p ON a.posture_id = p.id WHERE a.annotater_id = :annotaterId",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p ON a.posture_id = p.id
+            WHERE a.annotater_id = :annotaterId
+            """, nativeQuery = true)
     public List<AnnotationEntity> findByAnnotaterIdWithFilePath(@Param("annotaterId") Long annotaterId);
 
     @Transactional
     public Long countByAnnotaterId(Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p ON a.posture_id = p.id WHERE a.posture_id = :postureId",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p ON a.posture_id = p.id
+            WHERE a.posture_id = :postureId
+            """, nativeQuery = true)
     public List<AnnotationEntity> findByPostureIdWithFilePath(@Param("postureId") Long postureId);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p ON a.posture_id = p.id WHERE a.posture_id = :postureId AND a.annotater_id = :annotaterId",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p ON a.posture_id = p.id
+            WHERE a.posture_id = :postureId AND a.annotater_id = :annotaterId
+            """, nativeQuery = true)
     public AnnotationEntity findByPostureIdAndAnnotaterIdWithFilePath(
-        @Param("postureId") Long postureId,
-        @Param("annotaterId") Long annotaterId
-    );
+            @Param("postureId") Long postureId,
+            @Param("annotaterId") Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT a.* FROM annotations as a WHERE a.annotater_id = :annotaterId AND a.posture_id not in (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE) ORDER BY a.id ASC",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.* FROM annotations as a
+            WHERE a.annotater_id = :annotaterId AND a.posture_id NOT IN
+                (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE)
+            ORDER BY a.id ASC
+            """, nativeQuery = true)
     public List<AnnotationEntity> findProdByAnnotaterId(@Param("annotaterId") Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p on a.posture_id = p.id WHERE a.annotater_id = :annotaterId AND a.posture_id not in (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE) ORDER BY a.id ASC",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p on a.posture_id = p.id
+            WHERE a.annotater_id = :annotaterId AND a.posture_id NOT IN
+                (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE)
+            ORDER BY a.id ASC
+            """, nativeQuery = true)
     public List<AnnotationEntity> findProdByAnnotaterIdWithFilePath(@Param("annotaterId") Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT COUNT(1) FROM annotations as a WHERE a.annotater_id = :annotaterId AND a.posture_id in (SELECT p.id FROM postures as p WHERE p.is_sample = :isSample)",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT COUNT(1) FROM annotations as a
+            WHERE a.annotater_id = :annotaterId AND a.posture_id IN
+                (SELECT p.id FROM postures as p WHERE p.is_sample = :isSample)
+            """, nativeQuery = true)
     public Long countByAnnotaterIdAndIsSample(
-        @Param("annotaterId") Long annotaterId,
-        @Param("isSample") boolean isSample
-    );
+            @Param("annotaterId") Long annotaterId,
+            @Param("isSample") boolean isSample);
 
     @Transactional
-    @Query(
-        value = "SELECT a.* FROM annotations as a WHERE a.annotater_id = :annotaterId AND a.posture_id in (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE) ORDER BY a.id ASC",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.* FROM annotations as a
+            WHERE a.annotater_id = :annotaterId AND a.posture_id IN
+                (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE)
+            ORDER BY a.id ASC
+            """, nativeQuery = true)
     public List<AnnotationEntity> findSampleByAnnotaterId(@Param("annotaterId") Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a INNER JOIN postures as p on a.posture_id = p.id WHERE a.annotater_id = :annotaterId AND a.posture_id in (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE) ORDER BY a.id ASC",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT a.*, p.user_id, p.ex_created_at FROM annotations as a
+            INNER JOIN postures as p on a.posture_id = p.id
+            WHERE a.annotater_id = :annotaterId AND a.posture_id IN
+                (SELECT p.id FROM postures as p WHERE p.is_sample = TRUE)
+            ORDER BY a.id ASC
+            """, nativeQuery = true)
     public List<AnnotationEntity> findSampleByAnnotaterIdWithFilePath(@Param("annotaterId") Long annotaterId);
 
     @Transactional
-    @Query(
-        value = "SELECT p.is_sample FROM annotations as a INNER JOIN postures as p on a.posture_id = p.id WHERE a.id = :id",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT p.is_sample FROM annotations as a
+            INNER JOIN postures as p on a.posture_id = p.id
+            WHERE a.id = :id
+            """, nativeQuery = true)
     public boolean isSampleById(@Param("id") Long id);
 
     @Transactional
-    @Query(
-        value = "SELECT p.is_sample FROM annotations as a INNER JOIN postures as p on a.posture_id = p.id WHERE a.annotater_id = :annotaterId AND a.posture_id = :postureId",
-        nativeQuery = true
-    )
+    @Query(value = """
+            SELECT p.is_sample FROM annotations as a
+            INNER JOIN postures as p on a.posture_id = p.id
+            WHERE a.annotater_id = :annotaterId AND a.posture_id = :postureId
+            """, nativeQuery = true)
     public boolean isSampleByPostureIdAndAnnotaterId(
-        @Param("annotaterId") Long annotaterId,
-        @Param("postureId") Long postureId
-    );
+            @Param("annotaterId") Long annotaterId,
+            @Param("postureId") Long postureId);
 
 }
