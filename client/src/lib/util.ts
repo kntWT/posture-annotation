c8f813b5c95da3fe2cad91b6ac0a51df617aac17
kntWT <kento.wata0628@gmail.com>
2024-09-24 15:55:21 +0900

    feat(client): パスから画像を取得する関数追加

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -1,45 +1,54 @@
-import type { Posture } from "$api/generated";
+import type { Posture } from '$api/generated';
 
-const paths = ["original", "annotated"] as const;
-type Path = typeof paths[number];
+const paths = ['original', 'annotated'] as const;
+type Path = (typeof paths)[number];
 
 type DirectoryInfo = {
-    userId: number | "sample";
-    annotaterId?: number;
-    fileName: string;
-}
+	userId: number | 'sample';
+	annotaterId?: number;
+	fileName: string;
+};
 
 export const formatDate = (date: Date | undefined) => {
-    if (!date) {
-        return "";
-    }
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
-    const ms = date.getMilliseconds();
-    const pad = (num: number, digit: number) => num.toString().padStart(digit, "0");
-    return `${year}-${pad(month, 2)}-${pad(day, 2)}_${pad(hour, 2)}:${pad(minute, 2)}:${pad(second, 2)}.${pad(ms, 3)}`;
-}
+	if (!date) {
+		return '';
+	}
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const hour = date.getHours();
+	const minute = date.getMinutes();
+	const second = date.getSeconds();
+	const ms = date.getMilliseconds();
+	const pad = (num: number, digit: number) => num.toString().padStart(digit, '0');
+	return `${year}-${pad(month, 2)}-${pad(day, 2)}_${pad(hour, 2)}:${pad(minute, 2)}:${pad(second, 2)}.${pad(ms, 3)}`;
+};
 
-export const imageUrl = (info: DirectoryInfo, path: Path = "original") => {
-    const baseDir = path === "original"
-        ? `${import.meta.env.VITE_FILE_CLIENT_URL}/images/${path}/${info.userId}`
-        : `${import.meta.env.VITE_FILE_CLIENT_URL}/images/${path}/${info.userId}/${info.annotaterId ?? 1}`;
-    return `${baseDir}/${info.fileName}`;
-}
+export const imageUrl = (info: DirectoryInfo, path: Path = 'original') => {
+	const baseDir =
+		path === 'original'
+			? `${import.meta.env.VITE_FILE_CLIENT_URL}/images/${path}/${info.userId}`
+			: `${import.meta.env.VITE_FILE_CLIENT_URL}/images/${path}/${info.userId}/${info.annotaterId ?? 1}`;
+	return `${baseDir}/${info.fileName}`;
+};
 
-export const imageUrlFromPosture = (posture: Posture, annotaterId: number, path: Path = "original") => {
-    const info: DirectoryInfo = {
-        userId: posture.userId,
-        annotaterId,
-        fileName: formatDate(posture.exCreatedAt) + ".jpg"
-    };
-    return imageUrl(info, path);
-}
+export const imageUrlFromPath = (path: string) => {
+	return `${import.meta.env.VITE_FILE_CLIENT_URL}/images/${path}`;
+};
+
+export const imageUrlFromPosture = (
+	posture: Posture,
+	annotaterId: number,
+	path: Path = 'original'
+) => {
+	const info: DirectoryInfo = {
+		userId: posture.userId,
+		annotaterId,
+		fileName: formatDate(posture.exCreatedAt) + '.jpg'
+	};
+	return imageUrl(info, path);
+};
 
 export const toBearer = (token: string) => {
-    return `Bearer ${token}`;
-}
+	return `Bearer ${token}`;
+};
