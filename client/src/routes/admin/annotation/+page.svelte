0a02d45861a83daef8340985b6819bb5a8b3b700
kntWT <kento.wata0628@gmail.com>
2024-09-26 20:11:13 +0900

    feat(client): データのフィルタを仮実装

--- client/src/routes/admin/annotation/+page.svelte
+++ client/src/routes/admin/annotation/+page.svelte
@@ -4,18 +4,23 @@
 	import { goto } from '$app/navigation';
 	import type { PageData } from './$types';
 	import type { Header } from '$lib/components/admin/types/AnnotationSummaryTable';
+	import type { Option } from '$lib/components/dataIntercepter/types/Option';
+	import type { FilterOptionKeys } from '$lib/components/dataIntercepter/types/FilterOption';
+	import DataSortFilter from '$lib/components/dataIntercepter/DataSortFilter.svelte';
+	import { filterOptions } from '$lib/components/dataIntercepter/config';
 	export let data: PageData;
 	type Data = Omit<AnnotationSummaryByPosture, 'annotaterIds'> & {
 		diffNeckAngle: number;
 		count: number;
 		annotaterIds: string;
 	};
+	type Key = keyof Data;
 	const navigateToDetail = (data: Data) => {
 		goto(`${import.meta.env.VITE_BASE_PATH}/admin/annotation/detail?posture_id=${data.postureId}`, {
 			invalidateAll: true
 		});
 	};
-	const headers: Header<keyof Data>[] = [
+	const headers: Header<Key>[] = [
 		{
 			display: 'id',
 			key: 'postureId',
@@ -65,22 +70,88 @@
 		}
 	];
 
-	$: summary =
-		data.summary?.map((d) => {
-			return {
-				...d,
-				diffNeckAngle: d.avgNeckAngle - d.originalNeckAngle,
-				count: d.annotationIds.length,
-				annotaterIds: d.annotaterIds.toSorted().join(', ')
-			};
-		}) ?? [];
+	const optionTemplate: Option<Key>[] = [
+		{ label: '件数', key: 'count', type: 'number' },
+		{ label: 'もとの首の角度', key: 'originalNeckAngle', type: 'number' },
+		{ label: '角度の平均', key: 'avgNeckAngle', type: 'number' },
+		{ label: '角度の標準偏差', key: 'stdNeckAngle', type: 'number' },
+		{ label: '角度の差', key: 'diffNeckAngle', type: 'number' }
+	];
+	let filters: { value: string; key: Key | undefined; option: Option<Key>[] }[] = [];
+	let filterOptionKey: FilterOptionKeys = 'equal';
+
+	$: filteredSummary = () => {
+		let summary =
+			data.summary?.map((d) => {
+				return {
+					...d,
+					diffNeckAngle: d.avgNeckAngle - d.originalNeckAngle,
+					count: d.annotationIds.length,
+					annotaterIds: d.annotaterIds.toSorted().join(', ')
+				};
+			}) ?? [];
+		filters.forEach((filter) => {
+			const option = optionTemplate.find((o) => o.key === filter.key);
+			if (!option) return;
+			const key = option.key;
+
+			let value: unknown;
+			switch (option.type) {
+				case 'number':
+					value = Number(filter.value);
+					break;
+				case 'date':
+					value = new Date(filter.value).getTime();
+					break;
+				case 'string':
+					value = filter.value;
+					break;
+			}
+			summary = summary.filter((d) => {
+				const filterOpt = filterOptions.find((f) => f.key === filterOptionKey);
+				if (!filterOpt) return false;
+				const { key: currentKey } = filterOpt;
+				const t = typeof d[key];
+				switch (currentKey) {
+					case 'above':
+						return d[key] > (value as typeof t);
+					case 'greater':
+						return d[key] >= (value as typeof t);
+					case 'below':
+						return d[key] < (value as typeof t);
+					case 'less':
+						return d[key] <= (value as typeof t);
+					case 'equal':
+						return d[key] === value;
+					case 'notEqual':
+						return d[key] !== value;
+				}
+			});
+		});
+
+		return summary;
+	};
+
+	$: counts = {
+		display: filteredSummary().length,
+		total: data.summary?.length ?? 0
+	};
 </script>
 
 {#if !data.summary}
 	<p>データがありません</p>
 {:else}
 	<div class="wrapper">
-		<AnnotationSummaryByPostureTable {headers} data={summary} {navigateToDetail} />
+		<div class="container">
+			<DataSortFilter
+				{optionTemplate}
+				bind:filters
+				{filterOptions}
+				bind:filterOptionKey
+				bind:counts
+			/>
+		</div>
+		<AnnotationSummaryByPostureTable {headers} data={filteredSummary()} {navigateToDetail} />
 	</div>
 {/if}
 
@@ -101,6 +172,10 @@
 
 		@include mediaQuery('lg') {
 			padding: 32px 8px;
+		}
+
+		.container {
+			margin-bottom: 16px;
 		}
 	}
 </style>
