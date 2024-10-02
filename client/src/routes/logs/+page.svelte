e2ba8a5bff2214a6b773aa6b73f3edc272f87675
kntWT <kento.wata0628@gmail.com>
2024-10-02 15:13:44 +0900

    feat(client): アノテーション履歴ページをフィルタできるように

--- client/src/routes/logs/+page.svelte
+++ client/src/routes/logs/+page.svelte
@@ -6,13 +6,31 @@
 	import IconButton, { Icon } from '@smui/icon-button';
 	import { formatDate, imageUrl } from '$lib/util';
 	import type { AnnotationWithPosture } from '$api/generated';
+	import DataSortFilter from '$lib/components/dataIntercepter/DataSortFilter.svelte';
+	import type { Option } from '$lib/components/dataIntercepter/types/Option';
 
 	export let data: PageData;
 	const base = import.meta.env.VITE_BASE_PATH;
+	type Content = AnnotationWithPosture['annotation'] & {
+		originalNeckAngle: number;
+		diffNeckAngle: number;
+		userId: number;
+		fileName: string;
+	};
+	type Key = keyof Content;
+	type Data = {
+		prod: Content[];
+		sample: Content[];
+	};
+	type Count = {
+		display: number;
+		total: number;
+	};
 
-	const formatData = (d: AnnotationWithPosture) => {
+	const formatData = (d: AnnotationWithPosture): Content => {
 		return {
 			...d.annotation,
+			originalNeckAngle: d.posture.neckAngle,
 			diffNeckAngle: d.annotation.neckAngle - d.posture.neckAngle,
 			userId: d.posture.userId,
 			fileName: formatDate(d.posture.exCreatedAt) + '.jpg'
@@ -26,15 +44,42 @@
 						title: 'サンプルデータ',
 						data: data.sample.annotations.map(formatData),
 						open: data.sample.count === 0,
-						subPath: 'sample'
+						subPath: 'sample' as 'sample',
+						key: 'sample' as keyof Data
 					},
 					{
 						title: '本番データ',
 						data: data.prod.annotations.map(formatData),
-						open: data.prod.count === 0
+						open: data.prod.count === 0,
+						key: 'prod' as keyof Data
 					}
 				]
 			: [];
+	const optionTemplate: Option<Key>[] = [
+		{ key: 'id', label: 'ID', type: 'number', availableUiTypes: ['dropdown'] },
+		{ key: 'neckAngle', label: '首の角度', type: 'number', availableUiTypes: ['dropdown'] },
+		{
+			key: 'originalNeckAngle',
+			label: 'もとの首の角度',
+			type: 'number',
+			availableUiTypes: ['dropdown']
+		},
+		{ key: 'diffNeckAngle', label: '首の角度の差', type: 'number', availableUiTypes: ['dropdown'] },
+		{ key: 'userId', label: 'ユーザーID', type: 'number', availableUiTypes: ['dropdown'] },
+		{ key: 'createdAt', label: '作成日時', type: 'date', availableUiTypes: ['dropdown'] }
+	];
+
+	let formattedData: Data = {
+		prod: [...data.prod.annotations.map(formatData)],
+		sample: [...data.sample.annotations.map(formatData)]
+	};
+
+	let filteredData: Data = { ...formattedData };
+
+	$: counts = {
+		prod: { display: filteredData.prod.length, total: data.prod.count },
+		sample: { display: filteredData.sample.length, total: data.sample.count }
+	} as { [key in keyof Data]: Count };
 </script>
 
 <div class="wrapper">
@@ -55,14 +100,21 @@
 							<p>データがありません</p>
 						</Content>
 					{:else}
+						<DataSortFilter
+							{optionTemplate}
+							bind:data={formattedData[content.key]}
+							bind:counts={counts[content.key]}
+							on:updateData={(e) => (filteredData[content.key] = e.detail)}
+						/>
 						<LayoutGrid>
-							{#each content.data as annotation}
+							{#each filteredData[content.key] as annotation}
 								<Cell
 									class="card mdc-elevation-transition"
 									spanDevices={{ desktop: 2, tablet: 2, phone: 4 }}
 								>
 									<AnnotatedCard
 										{annotation}
+										originalNeckAngle={annotation.originalNeckAngle}
 										navigatePath={`${base}/annotate${content.subPath ? `/${content.subPath}` : ''}`}
 										imageSrc={imageUrl(
 											{
@@ -99,7 +151,6 @@
 			}
 
 			:global(.smui-paper__content) {
-				// background-color: $secondary-color;
 				background-color: grayscale($color: $base-color);
 			}
 		}
