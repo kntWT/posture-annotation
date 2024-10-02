392a16d43853459d0d03eaf335da30035b3ddff8
kntWT <kento.wata0628@gmail.com>
2024-10-02 15:59:45 +0900

    feat(client): アノテータごとのdetailでもフィlたできるように

--- client/src/routes/admin/user/detail/+page.svelte
+++ client/src/routes/admin/user/detail/+page.svelte
@@ -4,8 +4,44 @@
 	import type { PageData } from './$types';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import DataSortFilter from '$lib/components/dataIntercepter/DataSortFilter.svelte';
+	import type { AnnotationWithPosture } from '$api/generated';
+	import type { Option } from '$lib/components/dataIntercepter/types/Option';
 
 	export let data: PageData;
+	const POSTURE_PREFIX = 'posture_';
+	type PosturePrefix = typeof POSTURE_PREFIX;
+	type Content = AnnotationWithPosture['annotation'] & {
+		[K in keyof AnnotationWithPosture['posture'] as `${PosturePrefix}${string & K}`]: AnnotationWithPosture['posture'][K];
+	} & { _diffNeckAngle: number };
+	type Key = keyof Content;
+	type Count = {
+		display: number;
+		total: number;
+	};
+
+	const formatData = (d: AnnotationWithPosture): Content => {
+		return {
+			...d.annotation,
+			...Object.fromEntries(
+				Object.entries(d.posture).map(([key, value]) => [`${POSTURE_PREFIX}${key}`, value])
+			),
+			_diffNeckAngle: d.annotation.neckAngle - d.posture.neckAngle
+		};
+	};
+
+	const revertFormatData = (d: Content): AnnotationWithPosture => {
+		return {
+			annotation: Object.fromEntries(
+				Object.entries(d).filter(([key]) => !key.startsWith(POSTURE_PREFIX) && !key.startsWith('_'))
+			) as AnnotationWithPosture['annotation'],
+			posture: Object.fromEntries(
+				Object.entries(d)
+					.filter(([key]) => key.startsWith(POSTURE_PREFIX))
+					.map(([key, value]) => [key.slice(POSTURE_PREFIX.length), value])
+			) as AnnotationWithPosture['posture']
+		};
+	};
 
 	const handleClick = (id: number) => {
 		const annotaterId = parseInt($page.url.searchParams.get('annotater_id') || '');
@@ -20,19 +56,73 @@
 			invalidateAll: true
 		});
 	};
+
+	const optionTemplate: Option<Key>[] = [
+		{ key: 'id', label: 'ID', type: 'number', availableUiTypes: ['dropdown'] },
+		{ key: 'neckAngle', label: '首の角度', type: 'number', availableUiTypes: ['dropdown'] },
+		{
+			key: `${POSTURE_PREFIX}neckAngle`,
+			label: 'もとの首の角度',
+			type: 'number',
+			availableUiTypes: ['dropdown']
+		},
+		{
+			key: '_diffNeckAngle',
+			label: '首の角度の差',
+			type: 'number',
+			availableUiTypes: ['dropdown']
+		},
+		{
+			key: `${POSTURE_PREFIX}userId`,
+			label: 'ユーザーID',
+			type: 'number',
+			availableUiTypes: ['dropdown']
+		},
+		{ key: 'createdAt', label: '作成日時', type: 'date', availableUiTypes: ['dropdown'] }
+	];
+
+	let formattedData: Content[] = [...(data.data?.map(formatData) || [])];
+
+	let filteredData: Content[] = [...formattedData];
+
+	$: counts = {
+		display: filteredData.length,
+		total: formattedData.length
+	} as Count;
 </script>
 
-{#if !data.data || data.data.length === 0}
-	<p>データがありません</p>
-{:else}
-	<LayoutGrid>
-		{#each data.data as { annotation, posture }}
-			<Cell
-				class="card mdc-elevation-transition"
-				spanDevices={{ desktop: 4, tablet: 6, phone: 12 }}
-			>
-				<AnnotationDetail {annotation} {posture} onClick={() => handleClick(annotation.id)} />
-			</Cell>
-		{/each}
-	</LayoutGrid>
-{/if}
+<div class="wrapper">
+	{#if !data.data || data.data.length === 0}
+		<p>データがありません</p>
+	{:else}
+		<div class="container">
+			<DataSortFilter
+				bind:data={formattedData}
+				{optionTemplate}
+				bind:counts
+				on:updateData={(e) => (filteredData = e.detail)}
+			/>
+		</div>
+		<LayoutGrid>
+			{#each filteredData.map(revertFormatData) as { annotation, posture }}
+				<Cell
+					class="card mdc-elevation-transition"
+					spanDevices={{ desktop: 4, tablet: 6, phone: 12 }}
+				>
+					<AnnotationDetail {annotation} {posture} onClick={() => handleClick(annotation.id)} />
+				</Cell>
+			{/each}
+		</LayoutGrid>
+	{/if}
+</div>
+
+<style lang="scss" scoped>
+	.wrapper {
+		padding: 16px;
+
+		.container {
+			margin-bottom: 16px;
+			text-align: center;
+		}
+	}
+</style>
