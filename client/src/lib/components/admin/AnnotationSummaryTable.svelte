8df0b4e53a9e8465216ced958b79fd16509d2cb7
kntWT <kento.wata0628@gmail.com>
2024-10-03 19:23:49 +0900

    feat(client): pagenationを別コンポーネントに切り分けて，無限ロードできるように

--- client/src/lib/components/admin/AnnotationSummaryTable.svelte
+++ client/src/lib/components/admin/AnnotationSummaryTable.svelte
@@ -1,27 +1,15 @@
 <script lang="ts">
 	import type { Header } from './types/AnnotationSummaryTable';
 
-	import DataTable, { Head, Body, Row, Cell, Pagination } from '@smui/data-table';
-	import Select, { Option } from '@smui/select';
-	import IconButton from '@smui/icon-button';
-	import { Label } from '@smui/common';
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 	import ToggleImage from '../common/ToggleImage.svelte';
 	import { getHighlightColor, imageUrlFromPath } from '$lib/util';
 
 	type T = $$Generic<Recorc<string, number | string>>;
+	type PARAM = $$Generic;
 	export let data: T[];
 	export let headers: Header<keyof T>[];
 	export let navigateToDetail: (row: T) => void;
-
-	let currentPage = 0;
-	let rowsPerPage = 50;
-	$: start = currentPage * rowsPerPage;
-	$: end = Math.min(start + rowsPerPage, data.length);
-	$: slice = data.slice(start, end);
-	$: lastPage = Math.max(Math.ceil(data.length / rowsPerPage) - 1, 0);
-	$: if (currentPage > lastPage) {
-		currentPage = lastPage;
-	}
 </script>
 
 {#if !data}
@@ -36,7 +24,7 @@
 			</Row>
 		</Head>
 		<Body>
-			{#each slice as stats}
+			{#each data as stats}
 				<Row>
 					{#each headers as head}
 						<Cell
@@ -60,49 +48,6 @@
 				</Row>
 			{/each}
 		</Body>
-
-		<Pagination slot="paginate">
-			<svelte:fragment slot="rowsPerPage">
-				<Label>Rows Per Page</Label>
-				<Select variant="outlined" bind:value={rowsPerPage} noLabel>
-					<Option value={50}>50</Option>
-					<Option value={100}>100</Option>
-					<Option value={200}>200</Option>
-				</Select>
-			</svelte:fragment>
-			<svelte:fragment slot="total">
-				{start + 1}-{end} of {data.length}
-			</svelte:fragment>
-
-			<IconButton
-				class="material-icons"
-				action="first-page"
-				title="First page"
-				on:click={() => (currentPage = 0)}
-				disabled={currentPage === 0}>first_page</IconButton
-			>
-			<IconButton
-				class="material-icons"
-				action="prev-page"
-				title="Prev page"
-				on:click={() => currentPage--}
-				disabled={currentPage === 0}>chevron_left</IconButton
-			>
-			<IconButton
-				class="material-icons"
-				action="next-page"
-				title="Next page"
-				on:click={() => currentPage++}
-				disabled={currentPage === lastPage}>chevron_right</IconButton
-			>
-			<IconButton
-				class="material-icons"
-				action="last-page"
-				title="Last page"
-				on:click={() => (currentPage = lastPage)}
-				disabled={currentPage === lastPage}>last_page</IconButton
-			>
-		</Pagination>
 	</DataTable>
 {/if}
 
---
+++ client/src/lib/components/common/InfinitePagenation.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { Label, Pagination } from '@smui/data-table';
+	import IconButton from '@smui/icon-button';
+	import Select, { Option } from '@smui/select';
+
+	type T = $$Generic;
+	export let contents: T[];
+	export let displayData: T[];
+	export let isLast: boolean;
+
+	const dispatch = createEventDispatcher<{
+		loadMore: { page: number; size: number; refresh: boolean };
+	}>();
+
+	let currentPage = 0;
+	export let rowsPerPage = 50;
+
+	$: start = currentPage * rowsPerPage;
+	$: end = Math.min(start + rowsPerPage, contents.length);
+	$: lastPage = Math.ceil(contents.length / rowsPerPage) - (isLast ? 1 : 0);
+	const loadMore = (page: number, size: number, refresh: boolean) => {
+		if (page < 0 || page > lastPage) return;
+
+		dispatch('loadMore', { page: page, size: size, refresh });
+	};
+
+	$: if (rowsPerPage > contents.length) {
+		currentPage = 0;
+		loadMore(0, rowsPerPage, true);
+	}
+
+	$: {
+		displayData = contents.slice(start, end);
+	}
+</script>
+
+<div class="infinit-pagenation-container">
+	<Pagination slot="paginate">
+		<svelte:fragment slot="rowsPerPage">
+			<Label>Rows Per Page</Label>
+			<Select variant="outlined" bind:value={rowsPerPage} noLabel>
+				<Option value={50}>50</Option>
+				<Option value={100}>100</Option>
+				<Option value={200}>200</Option>
+				<Option value={500}>500</Option>
+				<Option value={1000}>1000</Option>
+			</Select>
+		</svelte:fragment>
+		<svelte:fragment slot="total">
+			{start + 1}-{end} of {contents.length}
+		</svelte:fragment>
+
+		<IconButton
+			class="material-icons"
+			action="first-page"
+			title="First page"
+			on:click={() => (currentPage = 0)}
+			disabled={currentPage === 0}>first_page</IconButton
+		>
+		<IconButton
+			class="material-icons"
+			action="prev-page"
+			title="Prev page"
+			on:click={() => currentPage--}
+			disabled={currentPage === 0}>chevron_left</IconButton
+		>
+		<IconButton
+			class="material-icons"
+			action="next-page"
+			title="Next page"
+			on:click={() => {
+				loadMore(++currentPage, rowsPerPage, false);
+			}}
+			disabled={currentPage === lastPage}>chevron_right</IconButton
+		>
+		<IconButton
+			class="material-icons"
+			action="last-page"
+			title="Last page"
+			on:click={() => {
+				for (let i = currentPage; i < lastPage; i++) {
+					loadMore(i, rowsPerPage, false);
+				}
+				currentPage = lastPage;
+			}}
+			disabled={currentPage === lastPage}>last_page</IconButton
+		>
+	</Pagination>
+</div>
+
+<style lang="scss" scoped>
+	.infinit-pagenation-container {
+		:global(.mdc-data-table__pagination) {
+			border: none;
+		}
+	}
+</style>
