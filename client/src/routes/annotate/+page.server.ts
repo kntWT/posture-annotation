acd4658fe9f03619cbf905a2cd409a0b31688bb4
kntWT <kento.wata0628@gmail.com>
2024-10-04 17:47:08 +0900

    feat(client): posture idをベースに間引いてrandomするように

--- client/src/routes/annotate/+page.server.ts
+++ client/src/routes/annotate/+page.server.ts
@@ -1,26 +1,29 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
-import { createPostureApi, userApi } from "$api";
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { createPostureApi, userApi } from '$api';
 import { toBearer } from '$lib/util';
 
 export const load: PageServerLoad = async ({ cookies, url }) => {
-    const token = cookies.get(`${import.meta.env.VITE_COOKIE_PREFIX}token`);
-    if (!token || token === '' || token === 'undefined') {
-        redirect(301, `${import.meta.env.VITE_BASE_PATH}/login`);
-    }
-    const id = url.searchParams.get("id") ?? null;
-    const postureApi = createPostureApi({ token });
-    try {
-        if (id && /^[0-9]+$/.test(id)) {
-            const posture = await postureApi.getPostureById({ id: parseInt(id) });
-            return { posture };
-        } else {
-            const user = await userApi.getUserByToken({ authorization: toBearer(token) });
-            const posture = await postureApi.getRandomPostureByAnnotaterId({ annotaterId: user.id });
-            return { posture };
-        }
-    } catch (e) {
-        console.error(e);
-        return { posture: null };
-    }
-}
+	const token = cookies.get(`${import.meta.env.VITE_COOKIE_PREFIX}token`);
+	if (!token || token === '' || token === 'undefined') {
+		redirect(301, `${import.meta.env.VITE_BASE_PATH}/login`);
+	}
+	const id = url.searchParams.get('id') ?? null;
+	const postureApi = createPostureApi({ token });
+	try {
+		if (id && /^[0-9]+$/.test(id)) {
+			const posture = await postureApi.getPostureById({ id: parseInt(id) });
+			return { posture };
+		} else {
+			const user = await userApi.getUserByToken({ authorization: toBearer(token) });
+			const posture = await postureApi.getRandomPostureByAnnotaterIdThinOutById({
+				annotaterId: user.id,
+				step: import.meta.env.VITE_THIN_OUT_POSTURE_STEP
+			});
+			return { posture };
+		}
+	} catch (e) {
+		console.error(e);
+		return { posture: null };
+	}
+};
--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -1,15 +1,16 @@
 interface ImportMetaEnv {
-    readonly VITE_API_SERVER_URL: string;
-    readonly VITE_API_CLIENT_URL: string;
-    readonly VITE_FILE_SERVER_URL: string;
-    readonly VITE_FILE_CLIENT_URL: string;
-    readonly VITE_COOKIE_PREFIX: string;
-    readonly VITE_SAMPLE_DATA_SIZE: number;
-    readonly VITE_PROD_DATA_SIZE: number;
-    readonly VITE_BASE_PATH: string;
-    readonly VITE_ADMIN_USER_IDS: string;
+	readonly VITE_API_SERVER_URL: string;
+	readonly VITE_API_CLIENT_URL: string;
+	readonly VITE_FILE_SERVER_URL: string;
+	readonly VITE_FILE_CLIENT_URL: string;
+	readonly VITE_COOKIE_PREFIX: string;
+	readonly VITE_SAMPLE_DATA_SIZE: number;
+	readonly VITE_PROD_DATA_SIZE: number;
+	readonly VITE_BASE_PATH: string;
+	readonly VITE_THIN_OUT_POSTURE_STEP: number;
+	readonly VITE_ADMIN_USER_IDS: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+	readonly env: ImportMetaEnv;
 }
